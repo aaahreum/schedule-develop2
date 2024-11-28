855ef8c5c0ecdfbd265d7b23f945e14c25413249
이아름 <xox_lol@naver.com>
2024-11-28 17:31:08 +0900

    init commit

---
+++ src/main/java/com/example/scheduleproject3/ScheduleProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduleproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleProject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scheduleproject3/ScheduleProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
